26c51df0d5a5f6f87e2efe0f72e975ed59b0795e
elskater98 <elskater98@gmail.com>
2020-10-24 19:24:14 +0200

    First contact

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learning/main_functions.dart';
 
-void main ()=> runApp(MaterialApp(
-  home: Home()
-));
+void main() => runApp(MaterialApp(home: HomeCard()));
 
-
-
-class Home extends StatelessWidget{
+class HomeCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('my first app'),
+        title: Text('User ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
       ),
-      body: Container(
-        color: Colors.grey,
-        padding: EdgeInsets.symmetric(),
-        margin: EdgeInsets.all(30.0),
-        child: Icon(Icons.mail,
-        color: Colors.blue,),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Text('+'),
-        onPressed: (){},
-        backgroundColor: Colors.red[600],
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/avatar.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text('Name'.toUpperCase(),
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+            SizedBox(height: 10.0),
+            Text('Chun-Li',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: 30.0),
+            Text('Level'.toUpperCase(),
+                style: TextStyle(color: Colors.grey, letterSpacing: 2.0)),
+            SizedBox(height: 10.0),
+            Text('10',
+                style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: 30.0),
+            Row(
+              children:<Widget> [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(
+                  width: 30.0,
+                ),
+                Text(
+                  'chun.lit@gmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
-
--- lib/main_functions.dart
+++
@@ -1,3 +0,0 @@
-void printSome(String name){
-  print(name);
-}
